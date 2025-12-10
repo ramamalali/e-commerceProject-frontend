f53b04fb6ab8b9c8710bfdfcb9b6549cd3cb70f7
Rama Al Ali <rama.muhammed.alali@gmail.com>
2025-12-10 10:50:55 +0000

    upload backend

---
+++ backend/config/middlewares.js
@@ -0,0 +1,33 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+   {
+     name: 'strapi::cors',
+  config: {
+    enabled: true,
+    origin: ['*'], 
+    headers: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    } 
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
